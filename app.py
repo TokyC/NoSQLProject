ad656fe1317a95f8c6b9717526e9668412d831e9
Quentin <quentinprs62@gmail.com>
2021-04-26 22:34:54 +0200

    save file

--- app.py
+++ app.py
@@ -1,54 +1,121 @@
-import streamlit as st
-from pymongo import MongoClient
-from elasticsearch import Elasticsearch
-import json
+from functools import wraps
 
+import streamlit as st
+import pymongo
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+from nltk.stem.snowball import SnowballStemmer
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.title("Crypto-currency")
-st.write("# My first APP \n"
-         "Hello world"
-         "")
-
-
-@st.cache(hash_funcs={MongoClient: id})
-def get_client():
-    return MongoClient("mongodb://127.0.0.1/admin")
+st.markdown("This application aims to inform you about the news of the crypto currency ")
 
 
 # -------------------------
 # Connexion
-#-------------------------
+# -------------------------
+
+@st.cache(hash_funcs={pymongo.MongoClient: id})
+def get_client():
+    return pymongo.MongoClient("mongodb://127.0.0.1/crypto")
+
 
 client = get_client()
 db = client["crypto"]
-collection = db.startup_log
 
-es = Elasticsearch(hosts=["localhost"])
-st.write(es.ping())
+st.sidebar.subheader("MongoDB:")
+coll_name = st.sidebar.selectbox("Select collection: ", db.list_collection_names())
 
-# index access/creation
-if not es.indices.exists(index="words"):
-    es.indices.create(index="words")
 
-# -------------------------
+def connexion(db_name, col_name):
+    conn = get_client()
+    return conn[db_name][col_name]
+
+
+articles = connexion("crypto", coll_name)
+
+
+def load_data():
+    df2 = pd.DataFrame(list(articles.find()))
+    return df2
+
+
+dataArticle = load_data()
+st.write(dataArticle)
+
+
+@st.cache(allow_output_mutation=True)
+def load_mongo_data_tweet():
+    df = pd.DataFrame(list(db.collectionTweet.find()))
+    return df
+
+
+dataTweet = load_mongo_data_tweet()
+# st.write("Showing data for Twitter: ")
+# st.write(dataTweet)
+
+
+fig, ax = plt.subplots()
+dataTweet = dataTweet[:20]
+ax = sns.barplot(x=dataTweet.likes, y=dataTweet.author, orient='h')
+# ax = dataTweet[:10].plot.barh(x='author', y='likes')
+plt.plot()
+plt.show()
+st.pyplot()
+
+st.markdown("## Display of the most present words  ")
+df = dataTweet["tweets"]
+
+list_of_all_sentences = [sentence for sentence in df]
+
+lines = []
+for sentence in list_of_all_sentences:
+    words = sentence.split()
+    for w in words:
+        lines.append(w)
+
+# Removing Punctuation
+lines = [re.sub(r'[^A-Za-z0-9]+', '', x) for x in lines]
+lines2 = []
+listWordBanned = ["the", "to", "for", "on", "just", 'how', "is", "a", "you", "Am", "Im", "I", "i", "and", "that", "of",
+                  "in", "this", "it", "be", "not", "have", "my", "we", "beautiful", "what", "no", "as", "host", "me",
+                  "with", "like", "your", "at", "do", "if", "too", "can", "know", "people", "about", "RT", "most",
+                  "are"]
+for word in lines:
+    if word != '' and word not in listWordBanned:
+        lines2.append(word)
+
+# This is stemming the words to their root
+# The Snowball Stemmer requires that you pass a language parameter
+s_stemmer = SnowballStemmer(language='english')
+stem = []
+for word in lines2:
+    stem.append(s_stemmer.stem(word))
+
+df = pd.DataFrame(stem)
+df = df[0].value_counts()
+
+df = df[:15]
+plt.figure(figsize=(10, 5))
+sns.barplot(df.values, df.index, alpha=0.8)
+plt.title('Top Words Overall')
+plt.ylabel('Word from Tweet', fontsize=12)
+plt.xlabel('Count of Words', fontsize=12)
+plt.show()
+st.pyplot()
+
 # Display query
 # -------------------------
 
-#For print all collection
+# For print all collection
 for coll in db.list_collection_names():
     st.write(coll)
 
-testBddTweet = [st.write("Test Tweet 1: ",test, "\n") for test in db.collectionTweet.find({"author": "TIME"})]
+testBddTweet = [st.write("Test Tweet : ", test, "\n") for test in db.collectionTweet.find({"author": "Jesse"})]
 
-testBddReddit =  [st.write("Test reddit :",test2, "\n") for test2 in db.collectionReddit.find({"Author": "ibelite"})]
+testBddReddit = [st.write("Test reddit :", test2, "\n") for test2 in db.collectionReddit.find({"Author": "ibelite"})]
 
-search_param = {
-    "query": {
-        "terms": {
-            "_id": [ 1234, 42 ] # find Ids '1234' and '42'
-        }
-    }
-}
-
-response = es.search(index="some_index", body=search_param)
-st.write(response)
+# st.write(db.collectionTweet.find()[100])
